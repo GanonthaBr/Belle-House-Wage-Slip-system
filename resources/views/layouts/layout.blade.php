3027056e4e945bddd9b36eae11d7c6d0253a4b80
Ganontha <bganontha@gmail.com>
2025-01-06 16:12:03 +0100

    Add favicon to layout: include logo as a site icon in the head section

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -3,6 +3,8 @@
 
 <head>
     <meta charset="UTF-8">
+    {{-- favicon --}}
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/logo.png') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
