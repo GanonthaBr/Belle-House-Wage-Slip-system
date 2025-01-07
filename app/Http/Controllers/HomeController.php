635dd0dd3271f24bd024bd5ccbdd5d775f1ecc28
Ganontha <bganontha@gmail.com>
2025-01-07 12:49:32 +0100

    disconnect the remove invoice service

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,17 +21,17 @@ class HomeController extends Controller
         return view('employees');
     }
 
-    protected $remoteInvoiceService;
+    // protected $remoteInvoiceService;
 
-    public function __construct(RemoteInvoiceService $remoteInvoiceService)
-    {
-        $this->remoteInvoiceService = $remoteInvoiceService;
-    }
+    // public function __construct(RemoteInvoiceService $remoteInvoiceService)
+    // {
+    //     $this->remoteInvoiceService = $remoteInvoiceService;
+    // }
 
-    public function list()
-    {
-        $invoices = $this->remoteInvoiceService->getAllInvoices();
+    // public function list()
+    // {
+    //     $invoices = $this->remoteInvoiceService->getAllInvoices();
 
-        return view('invoices.index', ['invoices' => $invoices]);
-    }
+    //     return view('invoices.index', ['invoices' => $invoices]);
+    // }
 }
