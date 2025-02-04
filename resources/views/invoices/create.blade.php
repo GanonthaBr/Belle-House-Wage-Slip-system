82a92a7ffd4283bd87568be9e55ce55bd0e8692c
Ganontha <bganontha@gmail.com>
2025-02-04 11:54:50 +0100

    Add create invoice view template

---
+++ resources/views/invoices/create.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.layout')
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <h1>Create Invoice</h1>
+            </div>
+        </div>
+    </div>
+@endsection
