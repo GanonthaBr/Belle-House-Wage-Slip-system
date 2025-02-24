8f6eeabd1c2594abaddc61f1e84ebea2a6360923
Ganontha <bganontha@gmail.com>
2025-02-24 17:51:58 +0100

    Add invoice creation functionality with validation and dynamic fields

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\WageSlip;
-
 use Illuminate\Http\Request;
 use App\Services\RemoteInvoiceService;
 
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 
 class HomeController extends Controller
 {
@@ -28,6 +29,74 @@ class HomeController extends Controller
         $this->remoteInvoiceService = $remoteInvoiceService;
     }
 
+    //create
+    public function create()
+    {
+        return view('invoices.create');
+    }
+    //store
+    public function store(Request $request)
+    {
+        try {
+            $request->validate([
+                'client_id' => 'required',
+                'facture_type' => 'required',
+                'number' => 'nullable',
+                'echeance' => 'required',
+                'tax' => 'required',
+                'type_tax' => 'nullable',
+                'topic',
+                'date',
+                'payment_mode',
+                'payment_mode' => 'required',
+            ]);
+            $data = $request->all();
+
+            $data['date'] = Carbon::now()->format('Y-m-d');
+
+
+            // Process designations
+            $designations = [];
+            foreach ($request->designation_title as $index => $title) {
+                $designations[] = [
+                    'designation_title' => $title,
+                    'designation_details' => $request->designation_details[$index],
+                    'designation_quantity' => $request->designation_quantity[$index],
+                    'designation_unit_price' => $request->designation_unit_price[$index],
+                ];
+            }
+            $data['designations'] = $designations;
+            //get the last added invoice and retrieve its number
+            $invoices = $this->remoteInvoiceService->getAllInvoices();
+            $lastInvoice = end($invoices);
+            $data['number'] = $lastInvoice ? $lastInvoice['number'] + 1 : 1;
+            // dd($data['number']);
+            //turn response to boolean
+            if ($data['tax'] == 'OUI') {
+                $data['tax'] = true;
+            } else {
+                $data['tax'] = false;
+            }
+            $payload = [
+                'client_id' => $data['client_id'],
+                'name' => $data['facture_type'],
+                'topic' => $data['topic'],
+                'echeance' => $data['echeance'],
+                'date' => $data['date'],
+                'payment_mode' => $data['payment_mode'],
+                'tax' => $data['tax'],
+                'number' => $data['number'],
+                'type_tax' => $data['type_tax'],
+                'designations' => $data['designations'],
+            ];
+
+            $invoices = $this->remoteInvoiceService->createInvoice($payload);
+            // dd($invoices);
+        } catch (\Throwable $e) {
+            // Handle the exception
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
     public function list()
     {
         $invoices = $this->remoteInvoiceService->getAllInvoices();
--- resources/views/invoices/create.blade.php
+++ resources/views/invoices/create.blade.php
@@ -1,3 +1,4 @@
+<!-- filepath: /c:/Users/DELL/Desktop/work files/BelleHouse/bulletin-salaire/resources/views/invoices/create.blade.php -->
 @extends('layouts.layout')
 @section('content')
 <div class="container">
@@ -7,15 +8,68 @@
         <div class="form-group">
             <label for="client_id">Client Id</label>
             <input type="number" class="form-control" id="client_id" name="client_id" required>
-        </div>
+        </div>    
         <div class="form-group">
-            <label for="date">Date</label>
-            <input type="date" class="form-control" id="date" name="date" required>
+            <label for="topic">Object</label>
+            <input type="text" class="form-control" placeholder="Object de la facture" id="topic" name="topic" required>
         </div>
+        <fieldset>
+        <div class="form-check">
+            <label for="facture" class="form-label"> <b>Type de Facture:</b> </label> <br>
+            <input class="form-check-input" type="radio" name="facture_type" id="facture" value="Facture">
+            <label class="form-check-label" for="facture"> Facture</label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="facture_type" id="Devis" value="Facture Proforma" checked>
+            <label class="form-check-label" for="Devis">Facture Proforma</label>
+        </div>
+        </fieldset>
         <div class="form-group">
-            <label for="invoice_name">Invoice Name</label>
-            <input type="text" class="form-control" id="invoice_name" name="invoice_name" required>
+            <label for="echeance">Echéance de paiement</label>
+            <input type="date" class="form-control" id="echeance" name="echeance" required>
         </div>
+        
+        <div class="form-group">
+            <label for="montant_avanc">Montant Avance</label>
+            <input type="text" class="form-control" id="montant_avanc" name="montant_avanc">
+        </div>
+        <fieldset>
+            <div class="form-check">
+                <label for="tax" class="form-label"> <b>Section Taxe</b> </label> <br>
+                <input type="radio" id="tax_oui" name="tax" class="form-check-input" value="oui" required>
+                <label class="form-check-label" for="tax_oui">OUI</label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="tax" id="tax_non" value="non" checked>
+                <label class="form-check-label" for="tax_non">NON</label>
+            </div>
+        </fieldset>
+        <fieldset id="typeTaxFieldset">
+            <label for="type_tax" class="form-label"> <b>Type de Taxe</b> </label> <br>
+            <div class="form-check">
+                <input type="radio" id="tva" name="type_tax" class="form-check-input" value="TVA" checked required>
+                <label class="form-check-label" for="tva">TVA</label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="type_tax" id="isb" value="ISB" required>
+                <label class="form-check-label" for="isb">ISB</label>
+            </div>
+        </fieldset>
+        <fieldset>
+            <label for="payment_mode" class="form-label"> <b>Mode de Paiement</b> </label> <br>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="payment_mode" id="en_liquide" value="En ESPÈCES">
+                <label class="form-check-label" for="en_liquide"> En ESPÈCES</label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="payment_mode" id="par_banque" value="PAR VERSEMENT">
+                <label class="form-check-label" for="par_banque"> PAR VERSEMENT</label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="payment_mode" id="par_banque" value="PAR CHEQUE" checked>
+                <label class="form-check-label" for="par_banque"> PAR CHEQUE</label>
+            </div>
+        </fieldset>
         <fieldset>
             <legend>Liste des Elements</legend>
             <div id="itemFieldsContainer">
@@ -32,66 +86,65 @@
                     <div class="mb-3">
                         <input type="number" class="form-control" name="designation_unit_price[]" placeholder="Prix Unitaire" required>
                     </div>
-                    <button type="button" onclick="removeItemField(this)">Remove</button>
+                    <button type="button" onclick="removeItemField(this)">Retirer</button>
                 </div>
             </div>
             <button type="button" id="addItemField">Ajouter un Element</button>
-            </fieldset>
+        </fieldset>
         <!-- Add other fields as necessary -->
-        <button type="submit" class="btn btn-primary">Creer Invoice</button>
+        <button type="submit" class="btn btn-primary">Create Invoice</button>
     </form>
 </div>
 <script>
+    //Tax
+    document.addEventListener('DOMContentLoaded', function() {
+        const taxOui = document.getElementById('tax_oui');
+        const taxNon = document.getElementById('tax_non');
+        const typeTaxFieldset = document.getElementById('typeTaxFieldset');
 
-        document.getElementById('addItemField').addEventListener('click', function() {
-
-            const container = document.getElementById('itemFieldsContainer');
-
-            const newItemField = document.createElement('div');
-
-            newItemField.classList.add('itemField');
-
-            newItemField.innerHTML = `
-
-       <div class="mb-3">
-
-                            <input type="text" class="form-control" name="itemNames[]" placeholder="Designation" required>
-
-                        </div>
-
-                        <div class="mb-3">
-
-                            <input type="text" class="form-control" name="itemDetails[]" placeholder="Bref details Details">
-
-                        </div>
-
-                        <div class="mb-3">
-
-                            <input type="number" class="form-control" name="itemQuantities[]" placeholder="Quantity" required>
-
-                        </div>
-
-                        <div class="mb-3">
-
-                            <input type="number" class="form-control" name="itemPrices[]" placeholder="Prix Unitaire" required>
-
-                        </div>
-
-        <button type="button" onclick="removeItemField(this)">Remove</button>
-
-    `;
-
-            container.appendChild(newItemField);
-
-        });
-
-
-
-        function removeItemField(button) {
-
-            button.parentElement.remove();
-
+        function toggleTypeTaxFieldset() {
+            if (taxOui.checked) {
+                typeTaxFieldset.style.display = 'block';
+            } else {
+                typeTaxFieldset.style.display = 'none';
+            }
         }
 
-    </script>
+        taxOui.addEventListener('change', toggleTypeTaxFieldset);
+        taxNon.addEventListener('change', toggleTypeTaxFieldset);
+        toggleTypeTaxFieldset(); // Initial check
+    });
+    //Items
+    document.getElementById('addItemField').addEventListener('click', function() {
+
+        const container = document.getElementById('itemFieldsContainer');
+
+        const newItemField = document.createElement('div');
+
+        newItemField.classList.add('itemField');
+
+        newItemField.innerHTML = `
+
+       <div class="mb-3">
+            <input type="text" class="form-control" name="designation_title[]" placeholder="Designation" required>
+        </div>
+        <div class="mb-3">
+            <input type="text" class="form-control" name="designation_details[]" placeholder="Bref details Details">
+        </div>
+        <div class="mb-3">
+            <input type="number" class="form-control" name="designation_quantity[]" placeholder="Quantity" required>
+        </div>
+        <div class="mb-3">
+            <input type="number" class="form-control" name="designation_unit_price[]" placeholder="Prix Unitaire" required>
+        </div>
+        <button type="button" onclick="removeItemField(this)">Retirer</button>
+    `;
+        container.appendChild(newItemField);
+    });
+
+    function removeItemField(button) {
+        button.parentElement.remove();
+
+    }
+</script>
 @endsection
